f127b7573631e791c7110afe3860faaf7b7c724a
Adeola Samuel <meetprecious1403@gmail.com>
2024-10-19 07:38:37 +0100

    create page layout

---
+++ app/components/Dashboard.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+function Dashboard() {
+  return (
+    <section>
+      <h1 className="text-4xl font-bold">Welcome to Host Flow</h1>
+    </section>
+  );
+}
+
+export default Dashboard;

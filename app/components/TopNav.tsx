c692ac43b3d8dc664db8ea070cb569ef0b054fea
Adeola Samuel <meetprecious1403@gmail.com>
2024-10-28 18:25:09 +0100

    increase logo size on nav

--- app/components/TopNav.tsx
+++ app/components/TopNav.tsx
@@ -5,7 +5,7 @@ function TopNav({ children }: { children: React.ReactNode }) {
   return (
     <nav className="border-b dark:border-none">
       <div className="flex px-5 py-3 items-center justify-between">
-        <Image src="/icons/full-logo.png" alt="logo" width={100} height={100} />
+        <Image src="/icons/full-logo.png" alt="logo" width={150} height={150} />
         {children}
       </div>
     </nav>
